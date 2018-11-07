517f0d94c7659475447927227b852c3394441c2b
Mmaud <maudlemao@gmail.com>
2018-11-07 15:46:00 +0100

    code well for good build

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello, Worl)
+        System.out.println("Hello, World");
 	}
 	public int multiplication(int a, int b) {
 		return a*b;
