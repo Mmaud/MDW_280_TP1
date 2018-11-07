021e908b853995616f83fc58190a81c15b444a22
Mmaud <maudlemao@gmail.com>
2018-11-07 15:40:13 +0100

    test build failure 3

--- src/Hello.java
+++ src/Hello.java
@@ -2,10 +2,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello, World !!!!!");
+        System.out.println("Hello, Worl)
 	}
 	public int multiplication(int a, int b) {
-		console.log(ok;)
 		return a*b;
 	}
 }
